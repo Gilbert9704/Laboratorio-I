cf1ba59beb9e151aaf39a777e2f941b5673cccb4
User <User@Gilbert>
2016-02-22 17:46:13 -0500

    Ejercicios

--- src/laboratorio/i/LaboratorioI.java
+++ src/laboratorio/i/LaboratorioI.java
@@ -16,7 +16,28 @@ public class LaboratorioI {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int cuenta_as = 1;
+        int contador = 1;
+        int blan = 1;
+        int num = 5;
+        char ast = '*';
+        String esp = "";
         
+        System.out.println();
+        
+        while (contador <= num){
+            blan = num - contador;
+                while (blan > 0){
+                    System.out.println(esp);
+                    blan = blan - 1;
+                }
+                while (cuenta_as < (2*contador)){
+                    System.out.println(ast);
+                    cuenta_as = cuenta_as + 1;
+                }
+                contador = contador + 1;
+                System.out.println();
+        }
        
        
     }
