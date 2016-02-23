11a0cdfd7eb9ba6f648eae3f031a630499f3d357
User <User@Gilbert>
2016-02-22 22:24:11 -0500

    Ejercicios

--- src/laboratorio/i/GuessingGame.java
+++ src/laboratorio/i/GuessingGame.java
@@ -32,6 +32,26 @@ public class GuessingGame {
         int guess = 0;
         int numGuesses = 1;
         
-    }
+        while (numGuesses > 7 || (guess == answer)){
+            System.out.println("Imagina un numero");
+            java.util.Scanner number = new java.util.Scanner(System.in);
+            guess = number.nextInt();
+                
+                if (guess < answer){
+                    System.out.println("Mayor");
+                } 
+                else if (guess > answer){
+                    System.out.println("Elevado");
+                }
+                else {
+                    System.out.println("Ganaste!");
+                }
+                numGuesses = numGuesses + 1;
+        }
+        if (numGuesses > 7){
+            System.out.println("Perdiste :(");
+        }
+                
+    }       
     
 }
